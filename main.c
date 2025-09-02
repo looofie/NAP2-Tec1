95c7d4d33713c3688255f7e11bf722e1d73fe17d
loofie <nando.luis.lf.12@gmail.com>
2025-09-01 23:51:03 -0300

    Menu criado

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h> //Precisa para limpar o terminal pela funcao system()
+
 
 int main(){
-    printf("teste");
-    getchar();
+    int menuOpc;
+    int mostraMenu = 1;
+                
+    while(mostraMenu){
+        system("cls");
+        printf("[ JOGO INCRIVEL SEM NOME AINDA ]\n---------------------------------\n");
+        printf("1. Comecar\n2. Continuar\n3. Reiniciar\n4. Sair\n");
+        printf("---------------------------------\nDigite o numero correspondente: ");
+        scanf("%d", &menuOpc);
+
+        if(menuOpc == 4){
+            mostraMenu = 0;
+        }
+    }
 }
